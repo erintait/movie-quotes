bddb039649267bf11a7afa358a4074750eb9b9c0
Erin Tait <erinmtait@gmail.com>
2018-11-28 15:38:26 -0800

    Completed project setup

---
+++ src/components/secret_list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className = "center">Secret Operatives List</h1>
+        <ol>
+            <li>Jim</li>
+            <li>Ana</li>
+            <li>Kath</li>
+            <li>Cait</li>
+            <li>Kenders</li>
+            <li>Day</li>
+            <li>Ria</li>
+        </ol>
+    </div>
+);
